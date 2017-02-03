04b405c5296a9de7810ec4aa298a8946ad1d917b
Rafael Chinelato Del Nero <rafacdelnero@gmail.com>
2017-02-03 15:00:35 -0200

    Mudança de assinatura de método;

--- amazon-test/src/soujava/DistancePairNumbers.java
+++ amazon-test/src/soujava/DistancePairNumbers.java
@@ -1,17 +1,54 @@
 package soujava;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 
 public class DistancePairNumbers {
 
-	public void calculateMinimalDistanceBetweenIndexes(List<Integer> mainListToSearchDistance, List<String> pairList) {
-		calculateMinimalDistanceBetweenIndexes(mainListToSearchDistance, pairList);
+	public List<Integer> calculateMinimalDistanceBetweenIndexes(List<Integer> mainListToSearchDistance, List<String> pairList) {
+		
+		for (String pair : pairList) {
+			
+			int[] pairsToBeFound = getPairNumbersAsInt(pair);
+			
+			findMinimalDistanceInList(mainListToSearchDistance, pairsToBeFound[0], pairsToBeFound[1]);
+		}
+		
+		return null;
+		
+	}
+	
+	private int[] getPairNumbersAsInt(String pair) {
+		String[] pairs = pair.split(" ");
+		
+		int firstNumberOfPair = Integer.parseInt(pairs[0]);
+		int secondNumberOfPair = Integer.parseInt(pairs[1]);
+		
+		return new int[]{firstNumberOfPair, secondNumberOfPair};
 	}
 
-	private static List<String> populatePairList(String... pairs) {
-		return new ArrayList<>(Arrays.asList(pairs));
+	private void findMinimalDistanceInList(List<Integer> mainListToSearchDistance, int firstNumberOfPair, int secondNumberOfPair) {
+		boolean isFistNumberFound = false;
+		boolean isSecondNumberFound = false;
+		
+		int firstPositionIndexFoun = 0;
+		
+		for (Integer numberOfMainList : mainListToSearchDistance) {
+			int distanceCount = 0;
+			
+			if (numberOfMainList.equals(firstNumberOfPair) && !isFistNumberFound) {
+				
+				
+				isFistNumberFound = true;
+			} else if (numberOfMainList.equals(secondNumberOfPair) && !isSecondNumberFound) {
+				isSecondNumberFound = true;
+			} else if (isFistNumberFound && isSecondNumberFound) {
+				
+			} else {
+				distanceCount++;
+			}
+		}
+		
 	}
+
 }
